089f542aa52c86a6eaad6b07afc6453ae23414eb
Champ <champr12@live.com>
2024-05-31 19:04:48 -0700

    changed remount_media.py: added container restart code

--- remount_media.py
+++ remount_media.py
@@ -13,11 +13,13 @@ def is_directory_empty(directory):
     # os.listdir lists the content in the specified directory
     return len(os.listdir(directory)) == 0
 
-# Loops over the directories list and check if the directories are empty
+# Loops over the directories list and check if the directories are empty, run the mount commands if the directories are empty
+# the mount commands are "rclone mount whatbox:/home/champr12/Movies /home/localusr/docker/plex/media/Movies --allow-other --daemon" and "rclone mount whatbox:/home/champr12/Shows /home/localusr/docker/plex/media/Shows --allow-other --daemon" 
 for directory in directories:
     if is_directory_empty(directory) and directory == "/home/localusr/docker/plex/media/Movies":
         subprocess.run(["rclone", "mount", "whatbox:/home/champr12/Movies", "/home/localusr/docker/plex/media/Movies", "--allow-other", "--daemon"])
     elif is_directory_empty(directory) and directory == "/home/localusr/docker/plex/media/Shows":
         subprocess.run(["rclone", "mount", "whatbox:/home/champr12/Shows", "/home/localusr/docker/plex/media/Shows", "--allow-other", "--daemon"])       
 
-    
+# Restart the Plex Docker container
+subprocess.run(["sudo","docker", "restart", "61a4023ebc29"])
